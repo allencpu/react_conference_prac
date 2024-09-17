c63e91246f893e6739267c439d0f1cfbc0ba5754
allencpu <allenproof@yahoo.com>
2024-09-16 21:07:14 -0700

    Update conferenceEvent.jsx
    Tested Code

--- conferenceEvent.jsx
+++ conferenceEvent.jsx
@@ -3,7 +3,7 @@ import "./ConferenceEvent.css";
 import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
-import { incrementAvQuantity } from "./avSlice";
+import { incrementAvQuantity, decrementAvQuantity } from "./avSlice";
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
@@ -31,11 +31,11 @@ const ConferenceEvent = () => {
         }
       };
     const handleIncrementAvQuantity = (index) => {
-        dispatch(incrementAvQuantity (index));
+        dispatch(incrementAvQuantity(index));
     };
 
     const handleDecrementAvQuantity = (index) => {
-        dispatch(decrementAvQuanity (index));
+        dispatch(decrementAvQuanity(index));
     };
 
     const handleMealSelection = (index) => {
@@ -57,10 +57,15 @@ const ConferenceEvent = () => {
           venueItems.forEach((item) => {
             totalCost += item.cost * item.quantity;
           });
+        } else if (section === "av") {
+            avItems.forEach((item) => {
+                totalCost += item.cost * item.quantity;
+            });
         }
         return totalCost;
       };
     const venueTotalCost = calculateTotalCost("venue");
+    const avTotalCost = calculateTotalCost("av");
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -162,20 +167,21 @@ const ConferenceEvent = () => {
                                 </div>
                                 <div className="addons_selection">
                                 {avItems.map((item, index) => (
-    <div className="av_data venue_main" key={index}>
-        <div className="img">
-            <img src={item.img} alt={item.name} />
-        </div>
-    <div className="text"> {item.name} </div>
-    <div> ${item.cost} </div>
-        <div className="addons_btn">
-            <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
-            <span className="quantity-value">{item.quantity}</span>
-            <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
-        </div>
-    </div>
-))}
+                             <div className="av_data venue_main" key={index}>
+                    <div className="img">
+                     <img src={item.img} alt={item.name} />
+                        </div>
+                         <div className="text"> {item.name} </div>
+                        <div> ${item.cost} </div>
+                        <div className="addons_btn">
+                        <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                        <span className="quantity-value">{item.quantity}</span>
+                    <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                    </div>
+                        </div>
+                            ))}
                                 </div>
+                                <div className="total_cost">Total Cost: {avTotalCost}</div>
                                 <div className="total_cost">Total Cost:</div>
 
                             </div>
@@ -217,3 +223,4 @@ const ConferenceEvent = () => {
 };
 
 export default ConferenceEvent;
+
